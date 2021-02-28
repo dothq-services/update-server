2c6584ff70b04a352eafe9f5217d6085d2b54559
Trevor Thalacker <trevor@trevorthalacker.me>
2021-02-28 12:45:12 -0800

    :sparkles: Start working on connecting to DB

--- public/index.ts
+++ public/index.ts
@@ -8,7 +8,13 @@ app.get("/", (req, res) => {
 });
 
 app.get('/update/1/:product/:version/:buildID/:buildTarget/:locale/:channel/update.xml', (req, res) => {
-    res.send('Product is set to ' + req.params.buildID)
+    // Ok, so basically, first, check for all releases that have the same channel, build target, and locale, and product.
+    // Then filter through until we find one that has the newest version
+    // If none exist, just show a blank update.xml
+    
+    //res.header("Content-Type", "text/xml");
+    //res.end(`<?xml version="1.0"?>
+    //<updates></updates>`)
 })
 
 app.listen(port, () => {
