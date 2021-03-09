b1f34972427a6beb18b8061334b4daeb85bfe603
Trevor Thalacker <trevor@trevorthalacker.me>
2021-03-08 16:54:11 -0800

    :bug: Fix bug w/ icon went to /signin instead of /

--- admin/src/components/Header/index.tsx
+++ admin/src/components/Header/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Button } from '@material-ui/core'
-import { useCookies } from 'react-cookie'
-import axios from 'axios';
+import Link from 'next/link'
 
 export const Header = ({
   uData,
@@ -12,14 +11,14 @@ export const Header = ({
         <nav className={`nav-nb`}>
           <div className={`nav-container`}>
             <div className={'nav-left'}>
-              <a href={'/'}>
-                <i className={'dot-icon'} />
-              </a>
+              <Link href={'/'}>
+                <a>
+                  <i className={'dot-icon'} />
+                </a>
+              </Link>
               <ul className={'nav-items'}>
                 <a href={'/updates'}>Updates</a>
                 <a href={'/updates/add'}>Add an Update</a>
-                <span />
-                <a href={'/targets'}>Targets</a>
               </ul>
             </div>
   
