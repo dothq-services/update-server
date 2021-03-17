d5d3214f63042a3403c281c5dbcd1e8561eb02a5
Trevor Thalacker <trevor@trevorthalacker.me>
2021-03-16 18:02:33 -0700

    :sparkles: Static file serving and index page

--- fs/index.ts
+++ fs/index.ts
@@ -1,11 +1,17 @@
 import express from 'express'
+import path from 'path'
+const serveIndex = require('serve-index')
 
 const app = express()
 const port = process.env.PORT || 9050
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
+
+app.use(serveIndex('public'))
+
+app.use(express.static('public'))
 
 app.listen(port, () => {
     console.log(`🚀 App listening at http://localhost:${port}`)
