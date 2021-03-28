d9aaf91cd1f9718beb7a154fe93b9ffc2e18fda7
Trevor Thalacker <trevor@trevorthalacker.me>
2021-03-27 18:06:52 -0700

    :sparkles: API requires authentication now

--- admin/src/pages/api/add/release.ts
+++ admin/src/pages/api/add/release.ts
@@ -1,9 +1,29 @@
 import { Releases } from '../../../lib/database';
 import Sequelize from 'sequelize'
+import axios from 'axios'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
+        // Verify all the values exist
+        const param = req.body;
+        if (
+            (param.name ||
+            param.product ||
+            param.channel ||
+            param.target ||
+            param.locale ||
+            param.version ||
+            param.displayVersion ||
+            param.buildID ||
+            param.whatsNewURL ||
+            param.releaseNotesURL ||
+            param.releaseFileURL ||
+            param.releaseFileSize ||
+            param.releaseFileChecksum) === undefined
+        ) {
+            return res.status(400).json({ error: 'missingParameters', message: 'A required parameter is missing' })
+        }
         // Get the ID
         let id = 0;
         const rel = await Releases.findAll({
@@ -11,35 +31,42 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             order: Sequelize.literal('id DESC'),
         })
         id = +rel[0].dataValues.id + 1; // generate an ID that is one higher than the previous one
-        // Obviously, we want to verify that the token is valid
-        // Next, verify that all the columns have valid info to add
-        // then we submit it to the DB, which sends it to the public server :)
-        const target = Releases.create({ 
-            name: req.body.name,
-            product: req.body.product,
-            channel: req.body.channel,
-            target: req.body.target,
-            locale: req.body.locale,
-            version: req.body.version,
-            displayVersion: req.body.displayVersion,
-            buildID: req.body.buildID,
-            whatsNewURL: req.body.whatsNewURL,
-            releaseNotesURL: req.body.releaseNotesURL,
-            releaseFileURL: req.body.releaseFileURL,
-            releaseFileSize: req.body.releaseFileSize,
-            releaseFileChecksum: req.body.releaseFileChecksum,
-            releaseType: 'minor',
-            id: 0,
-        }).then(() => {
-            res.status(200).json({ success: 'Release Added'})
-        }).catch((e) => {
-            console.log(e)
-            res.status(400).json({ error: e })
-        }); 
-        target;
+        // Let's verify the Token again
+        await axios.post(`http://${req.headers.host}/api/id/getOrganizations`, {
+            token: param.token
+        }).then((values) => { 
+            if (values.data.success === 'userValid') {
+                // Create a new release on the Database
+                const target = Releases.create({ 
+                    name: req.body.name,
+                    product: req.body.product,
+                    channel: req.body.channel,
+                    target: req.body.target,
+                    locale: req.body.locale,
+                    version: req.body.version,
+                    displayVersion: req.body.displayVersion,
+                    buildID: req.body.buildID,
+                    whatsNewURL: req.body.whatsNewURL,
+                    releaseNotesURL: req.body.releaseNotesURL,
+                    releaseFileURL: req.body.releaseFileURL,
+                    releaseFileSize: req.body.releaseFileSize,
+                    releaseFileChecksum: req.body.releaseFileChecksum,
+                    releaseType: 'minor',
+                    id: id,
+                }).then(() => {
+                    res.status(200).json({ success: 'releaseAdded', message: 'Release Sucessfully Added' })
+                }).catch((error) => {
+                    res.status(400).json({ error: 'serverError', message: error })
+                }); 
+                return target;
+            }
+        }).catch((error) => {
+            // Token auth didn't work
+            return res.status(400).json({ error: 'authError', message: error })
+        })
     } else {
         // Handle any other HTTP method
-        res.status(400).json({ error: 'Invalid Request Type' })
+        return res.status(400).json({ error: 'invalidMethod', message: 'Invalid Request Method. Allowed Methods: POST' })
     }
 }
   
